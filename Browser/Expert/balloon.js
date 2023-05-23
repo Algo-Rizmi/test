f91dcaad463782d5e0bac9904e50208fba08f230
Mohammed Sharafi <37874836+Pr1ntGn0me@users.noreply.github.com>
2023-05-24 01:46:50 +0200

    balloon explosion

--- Browser/Expert/balloon.js
+++ Browser/Expert/balloon.js
@@ -1,7 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   let image = document.getElementById("myImage");
 
-  image.addEventListener("click", () => {
-    alert("Bigger");
+  document.addEventListener("keydown", (event) => {
+    // rest of your code...
+    // Get the current width and height of the image.
+    let width = image.offsetWidth;
+    let height = image.offsetHeight;
+
+    if (event.key === "ArrowUp") {
+      width = width * 1.1;
+      height = height * 1.1;
+
+      if (width > 280 || height > 280) {
+        image.src = "explosion.gif";
+      } else {
+        image.style.width = width + "px";
+        image.style.height = height + "px";
+      }
+    } else if (event.key === "ArrowDown") {
+      width = width * 0.9;
+      height = height * 0.9;
+      image.style.width = width + "px";
+      image.style.height = height + "px";
+    }
+    console.log(width + " " + height);
   });
 });
